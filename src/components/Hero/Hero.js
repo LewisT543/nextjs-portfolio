88695cb06be73c785c62b675736b541dfb5b7d9e
Lewis <lewisthornton@btinternet.com>
2022-12-29 16:48:03 +0000

    Initial commit, (header, hero, projects)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        This is a baseline react/next.js app, demonstrating what a very small amount of work can do using these tools
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'} >Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
