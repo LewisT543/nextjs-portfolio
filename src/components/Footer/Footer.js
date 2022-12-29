1c05376879b2d0a5eb314b8cd652a015ca9befa5
Lewis <lewisthornton@btinternet.com>
2022-12-29 17:33:07 +0000

    Finished portfolio project - needs refactor, some ugly usage of components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,32 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:111-111-1111'>111-111-1111</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href='mailto:contact@email.com'>contact@email.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Doing the needful since 1997</Slogan>
+        </CompanyContainer>
+        <SocialIcons href="https://github.com">
+          <AiFillGithub size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://linkedin.com">
+          <AiFillLinkedin size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://instagram.com">
+          <AiFillInstagram size="3rem"/>
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
